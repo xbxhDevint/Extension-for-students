88d2f3125d77e651b2063033dedd3d8f43407fa1
Devint <U23ayo26@uchon.es.kr>
2024-10-17 13:09:30 +0900

    Update index.js

--- index.js
+++ index.js
@@ -7,13 +7,14 @@ function clickElement(selector) {
         console.error('어딧지 지스팟이');
     }
 }
-
+clickElement('a.btn.btn-primary.btn-block.btn-opt-start');
 for (i = 0; i < Math.floor(study_data.length / 10); i++) {
   for (i = 0; i < 10; i++) {
     clickElement('.CardItem .card-cover');
     setTimeout(() => clickElement('.btn-success'), 200);
     setTimeout(() => console.log("뷁"), audioTimer);
   }
+  clickElement('a.btn.btn-xl.btn-primary.shadow.mw-250.btn-study-end-next-section2');
 }
 if (study.data.length % 10 < 5) {
   for (i = 0; i < study_data.length % 10; i++) {
