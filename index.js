0fb5c70f1c90a7147dbeb10dcbdd08c977cd4773
Devint <U23ayo26@uchon.es.kr>
2024-10-17 13:15:33 +0900

    Update index.js

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ function clickElement(selector) {
 }
 clickElement('a.btn.btn-primary.btn-block.btn-opt-start');
 for (i = 0; i < Math.floor(study_data.length / 10); i++) {
-  for (i = 0; i < 10; i++) {
+  for (j = 0; j < 10; j++) {
     clickElement('.CardItem .card-cover');
     setTimeout(() => clickElement('.btn-success'), 200);
     setTimeout(() => console.log("뷁"), audioTimer);
@@ -17,7 +17,7 @@ for (i = 0; i < Math.floor(study_data.length / 10); i++) {
   clickElement('a.btn.btn-xl.btn-primary.shadow.mw-250.btn-study-end-next-section2');
 }
 if (study.data.length % 10 < 5) {
-  for (i = 0; i < study_data.length % 10; i++) {
+  for (k = 0; k < study_data.length % 10; k++) {
     clickElement('.CardItem .card-cover');
     setTimeout(() => clickElement('.btn-success'), 200);
     setTimeout(() => console.log("뷁"), audioTimer);
